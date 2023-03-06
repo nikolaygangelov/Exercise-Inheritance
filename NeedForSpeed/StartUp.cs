1f2959df001d22ac9fd706412bb9e5fa7cc7dcd9
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2023-03-06 20:47:37 +0200

    Add files via upload

---
+++ NeedForSpeed/StartUp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car(300, 5);
+            Console.WriteLine(car.FuelConsumption);
+        }
+    }
+}
